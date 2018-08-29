91b1d2a1dc0e0365684c64a9a8e1de7107d3858d
Timothy Rasey <tdrasey91@gmail.com>
2018-08-29 10:24:55 -0500

    Add project files.

---
+++ Lab 3 BMI Calculator/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Lab_3_BMI_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double dblHeight;
+            double dblWeight;
+            double dblBMI;
+
+            //user input
+            Console.WriteLine("Enter your Height in inches: ");
+
+            //validate
+            while(!double.TryParse(Console.ReadLine(), out dblHeight) || dblHeight < 0)
+            {
+                Console.WriteLine("Not valid. Must be a positive rational number.");
+            }
+
+
+            Console.WriteLine("Enter your Weight in pounds: ");
+
+            //validate
+            while(!double.TryParse(Console.ReadLine(), out dblWeight) || dblWeight < 0)
+            {
+                Console.WriteLine("Not valid. Must be a positive rational number.");
+            }
+           
+
+            dblBMI = (dblWeight * 703) / (dblHeight * dblHeight);
+
+            Console.WriteLine("Your BMI is " + dblBMI);
+
+
+
+            if(dblBMI < 18.5)
+            {
+                Console.WriteLine("BMI < 18.5 - Underweight");
+            }
+            else if(dblBMI <= 24.9)
+            {
+                Console.WriteLine("BMI between 18.5 and 24.9 - Normal");
+            }
+            else if(dblBMI<= 29.9)
+            {
+                Console.WriteLine("BMI between 25 and 29.9 - Overweight");
+            }
+            else
+            {
+                Console.WriteLine("BMI >30 - Obese");
+            }
+            Console.ReadLine();
+        }
+    }
+}
